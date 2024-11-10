3cc5c5c6cd2fdb9b184e46a0f1f6f6bff59d6a8d
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-11-10 17:35:08 +0800

    finish version

--- post/forms.py
+++ post/forms.py
@@ -5,3 +5,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'category']
+    def save(self, commit=True):
+        # 獲取表單的實例
+        post = super().save(commit=False)
+        
+        # 將 poster 設置為當前使用者
+        post.poster = self.user  # 假設 `self.user` 是當前用戶
+
+        # 如果需要立即保存，可以設置 commit=True
+        if commit:
+            post.save()
+        return post

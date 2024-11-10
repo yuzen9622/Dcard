e2cc0c59ae9b2537f5db639bfb958208b204cf33
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-11-10 18:59:49 +0800

    bug

--- myapp/settings.py
+++ myapp/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "/var/www/static/",
     
-# ]
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

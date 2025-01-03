48f769d577c3ea6d4dd930406ec3ede61e297c9a
yuzen9622 <oscar48079@gmail.com>
2025-01-03 23:22:20 +0800

    新增留言功能以及留言歷史紀錄查詢

--- post/models.py
+++ post/models.py
@@ -22,3 +22,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    poster = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content

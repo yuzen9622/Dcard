d3dbbe4b5dd907e234a9d8a432f5ea41877d4f1c
yuzen9622 <oscar48079@gmail.com>
2025-01-05 17:00:26 +0800

    vercel deploy

--- myapp/settings.py
+++ myapp/settings.py
@@ -117,7 +117,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
